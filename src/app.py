f9d0f1bff7419906a1f13ecf719683aea953e017
LaHuella <german.dosaula@gmail.com>
2024-12-13 20:38:05 +0000

    Trabajo Terminado

--- src/app.py
+++ src/app.py
@@ -11,9 +11,33 @@ from datastructures import FamilyStructure
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
-
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+
+John = {
+    "first_name": "John",
+    "last_name": jackson_family.last_name,
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+}
+
+Jane = {
+    "first_name": "Jane",
+    "last_name": jackson_family.last_name,
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}
+
+Jimmy = {
+    "first_name": "Jimmy",
+    "last_name": jackson_family.last_name,
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -26,17 +50,45 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+    try:
+        return jsonify(jackson_family._members), 200
+    except Exception as e:
+        return jsonify({"error": "Internal server error", "message": str(e)}), 500
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member/<int:id>', methods=['GET'])
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
 
+@app.route('/member', methods=['POST'])
+def create_member():
+    try:
+        new_member = request.json
 
-    return jsonify(response_body), 200
+        if not new_member or not all(k in new_member for k in ('id', 'first_name', 'last_name', 'age', 'lucky_numbers')):
+            return jsonify({"error": "Invalid request body"}), 400
+
+        if any(m['id'] == new_member['id'] for m in jackson_family._members):
+            return jsonify({"error": "Member with this ID already exists"}), 400
+
+        jackson_family._members.append(new_member)
+        return jsonify(new_member), 200
+    except Exception as e:
+        return jsonify({"error": "Internal server error", "message": str(e)}), 500
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_single_member(id):
+    member = jackson_family.get_member(id)
+ 
+    if member:
+        jackson_family.delete_member(id)
+        return jsonify({"message": f"Member deleted successfully: {member}"}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
